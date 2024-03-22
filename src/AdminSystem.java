dfa12540aa9c145a37c23496238130a5d27a546f
Torstein <141224356+Trind03@users.noreply.github.com>
2024-03-22 05:44:49 +0100

    Update
    Added new packages to the project. And init classes for basic operation, with function to render choices in main menu.
    
    Co-Authored-By: Eyes of The Serpent <141904493+eyesoftheserpent@users.noreply.github.com>

--- src/AdminSystem.java
+++ src/AdminSystem.java
@@ -1,18 +1,100 @@
+import java.util.*;
+import java.util.ArrayList;
+import sas.Student;
+public class AdminSystem
+{
+    ArrayList<Student> students = new ArrayList<Student>();
+    char input;
+    final static String[] cmds  = { //commands
+            "Create a student",
+            "Select student",
+            "View student",
+            "View all students",
+            "Delete selected student\n",
 
+            "Create subject",
+            "Select subject",
+            "View subject",
+            "View all subjects",
+            "Delete selected subject\n",
 
-public class AdminSystem {
+            "Create professor",
+            "Select professor",
+            "View professor",
+            "View all professors",
+            "Delete selected subject\n",
 
+            "Software version",
+            "Exit software"
+    };
 
-    /*
-    Assign/create a student to a subject
-    Assign/create a Teacher to a subject
-    Assign/create a subject
-     */
+    public void menu()
+    {
 
+            System.out.flush();
+            System.out.println("**** Main Menu ****\n");
+            for (int i = 0; i != cmds.length; i++)
+            {
+                System.out.println((i++) + ". " + cmds[i]);
+            }
+            System.out.print("Field: ");
+            String userInput = inputUser(); //<-- String eller int
+            System.out.println(userInput);
+            switch (userInput)
+            {
 
+                case "1":
+                    menu();
+                case "2":
+                    System.exit(0);
+                case "3":
+                    menu();
+                case "4":
+                    System.exit(0);
+                case "5":
+                    menu();
+                case "6":
+                    System.exit(0);
+                case "7":
+                    menu();
+                case "8":
+                    System.exit(0);
+                case "9":
+                    menu();
+                case "10":
+                    System.exit(0);
 
+                case "11":
+                    menu();
+                case "12":
+                    System.exit(0);
 
-    public void menu(){
-        System.out.println("Hello");
+                case "13":
+                    menu();
+                case "14":
+                    System.exit(0);
+
+                case "15":
+                    menu();
+                case "16":
+                    System.exit(0);
+                case "17":
+                System.exit(0);
+                default:
+                    menu();
+            }
+
     }
+
+    public String inputUser(){
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        scanner.close();
+        return switch (userInput) {
+            case null -> inputUser();
+            case "" -> inputUser();
+            default -> userInput;
+        };
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,9 @@
 
-public class Main {
+public class Main
+{
     private static final AdminSystem AdminSystem = new AdminSystem();
-    public static void main(String[]arr) {
+    public static void main(String[]arr)
+    {
         AdminSystem.menu();
     }
 }
-
-/*
-Student <-- Studenten/Personen
-SubjectID <-- Fag/Fagkode
-Teacher/Professor <-- Lærer/Professor
-*/
-
-/*
-Student
-*/
-
-/*
-SubjectID
-*/
-
-/*
-Professor
-*/
