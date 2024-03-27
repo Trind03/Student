4b58ff7eecf0280486f1ab4fc126f9d0ef2812d6
Torstein <torsrind@icloud.com>
2024-03-27 18:56:47 +0100

    Update and bugfixes
    Added new functionality, and refactored loop to print all available commands. And added array of available subjects.

---
+++ src/src/sas/Professor.java
@@ -0,0 +1,13 @@
+package sas;
+
+public class Professor// implements ICommonReference
+{
+    String Name;
+    int Age;
+    Professor(String name,int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+}
